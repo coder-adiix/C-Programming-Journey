b4803fa473905a08fee23d061338ad031b682068
Aditya kalambe <adityakalambe83@gmail.com>
2026-08-25 23:41:29 +0530

    Add files via upload

---
+++ prob.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    /* x = 5*2-2*3 */
+    int x = 5*2-2*3;
+    printf(" the value of x is %d \n",x);
+    return 0;
+}
---
+++ squ.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+     int side ;
+    printf(" Enter the side of the square =");
+    scanf("%d",&side);
+    printf(" The area of the square is %d", side*side);
+
+    getch();
+}
